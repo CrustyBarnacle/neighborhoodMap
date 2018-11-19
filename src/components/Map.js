9aa0d45c4af6a01d1fcb20bee7ee8e753e2ff840
Mario Burgos <CrustyBarnacle@users.noreply.github.com>
2018-11-19 02:24:02 -0800

    Updates  Map.js
    Renames Map component to myMap :-p

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
+import { Map } from "google-maps-react";
 
-class Map extends React.Component {
+class myMap extends React.Component {
   state = {
     zoom: 14,
-    center: {
-      lat: 37.774929,
-      lng: -122.419416
-    }
+    lat: 37.774929,
+    lng: -122.419416
   };
 
   loadMap() {
@@ -14,8 +13,8 @@ class Map extends React.Component {
     const mapRef = this.refs.map;
 
     this.map = new google.maps.Map(ReactDOM.findDOMNode(mapRef), {
-      center: center,
-      zoom: zoom
+      center: this.state.center,
+      zoom: this.state.zoom
     });
   }
 
