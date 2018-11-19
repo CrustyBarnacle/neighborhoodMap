abfb98dad29f5196dbd00bfaa081b31f43483632
Mario Burgos <CrustyBarnacle@users.noreply.github.com>
2018-11-19 01:45:49 -0800

    Adds App.js
    Main application JS file. Returns main div and Container.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Container from "./components/Container";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+        <h2>Get some coding done! :-p</h2>
+        <Container />
+      </div>
+    );
+  }
+}
+
+export default App;
